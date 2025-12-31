50df636747b969fac9060ba511e029e96799b38c
THENEVERDONE <163352801+JasmineODaniel@users.noreply.github.com>
2025-12-31 06:14:19 -0800

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-analytics.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
-
+ 
   // Your web app's Firebase configuration 
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
@@ -21,5 +21,6 @@
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
 </script>
+
 
 
