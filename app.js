9a50ee66676d1ec6be9c8a5981400b83b548e5c3
THENEVERDONE <163352801+JasmineODaniel@users.noreply.github.com>
2026-01-01 06:22:03 -0800

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
   // https://firebase.google.com/docs/web/setup#available-libraries
  
   // Your web app's Firebase configuration 
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional 
   const firebaseConfig = {
     apiKey: "AIzaSyB2qKFaqsibIOWDgemNunl5oNAZiJKZ9vk",
     authDomain: "nizzebook.firebaseapp.com",
@@ -21,6 +21,7 @@
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
 </script>
+
 
 
 
